fd60e2f24b6d4056ee09e0a12d70d0e4e42afea2
simon <simonlerou@gmail.com>
2023-05-07 16:03:22 -0400

    fin de journee

---
+++ code/CMakeFiles/PythPyth.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PythPyth.dir/src/PythPyth.cpp.o"
+  "CMakeFiles/PythPyth.dir/src/PythPyth.cpp.o.d"
+  "PythPyth"
+  "PythPyth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PythPyth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
